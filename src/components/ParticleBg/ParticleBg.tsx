fb83941aed134c37ee615b6fb8b9a613a6347ab6
Hecker <julianheckerdev@gmail.com>
2023-05-22 22:59:45 -0400

    particle background

---
+++ src/components/ParticleBg/ParticleBg.tsx
@@ -0,0 +1,23 @@
+import { useCallback } from 'react';
+import Particles from 'react-particles';
+import type { Container, Engine } from 'tsparticles-engine';
+import { loadFull } from 'tsparticles';
+
+import options from './particles.json';
+
+export const ParticleBg = () => {
+  const particlesInit = useCallback(async (engine: Engine) => {
+    await loadFull(engine);
+  }, []);
+
+  return (
+    <Particles
+      id="tsparticles"
+      url=""
+      init={particlesInit}
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      options={options}
+    />
+  );
+};
