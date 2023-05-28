f6520c2780432baf54266171698da111686db77e
Alex Monteverde <alexmo@avi.com>
2023-05-28 19:40:23 -0400

    Updating card, and input

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
 import { Box, Button, Stack } from '@mui/material';
 import { ParticleBg } from './components/ParticleBg';
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
+import './App.css';
 
 function App() {
   const [tab, setTab] = useState(0);
+  const [directory, setDirectory] = useState<FileList>();
+  const [directoryName, setDirectoryName] = useState<string>("");
+  const ref = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (ref.current !== null) {
+      ref.current.setAttribute("directory", "");
+      ref.current.setAttribute("webkitdirectory", "");
+    }
+  }, [ref]);
+
+  const handleFileInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const theFiles = e.target.files;
+    const relativePath = theFiles === null ? "" : theFiles[0].webkitRelativePath;
+    const folderName = relativePath.split("/");
+    setDirectoryName(folderName[0]);
+    setDirectory(e.target.files === null ? undefined : e.target.files)
+  }
 
   return (
   <div>
@@ -15,7 +34,7 @@ function App() {
         left: "50%", 
         msTransform: "translate(-50%, -50%)", 
         transform: "translate(-50%, -50%)", 
-        width: "50%",
+        width: "40%",
         height: "50%"}}>
       <Stack direction={"column"} 
              alignItems={"center"}
@@ -23,37 +42,37 @@ function App() {
              padding={"20px"}
              height={"100%"}
              borderRadius={"4px"}
-             bgcolor={"orange"}>
+             className="glass-box">
         <Stack direction={"row"}>
           <Button onClick={()=>setTab(0)} 
-                  sx={{color: tab === 0 ? "#383582" : "#FAFAFA",
-                       borderBottom: tab === 0 ? "2px solid #383582" : "none",
-                       borderBottomLeftRadius: "0",
-                       borderBottomRightRadius: "0"
-                  }}>
+                  className={`${tab === 0 ? "action-button--active" : "action-button"}`}>
             Deploy
           </Button>
           <Button onClick={()=>setTab(1)}
-                  sx={{color: tab === 1 ? "#383582" : "#FAFAFA",
-                       borderBottom: tab === 1 ? "2px solid #383582" : "none",
-                       borderBottomLeftRadius: "0",
-                       borderBottomRightRadius: "0"
-                  }}>
+                  className={`${tab === 1 ? "action-button--active" : "action-button"}`}>
             View
           </Button>
         </Stack>
         {tab === 0 ?
             <Box sx={{height: "100%",
+                      width: "100%",
                       display: "flex",
                       alignItems: "center",
                       justifyContent: "center",
-                      border: "2px dashed #181818",
+                      border: "2px dashed #FAFAFA",
                       borderRadius: "4px"
                  }}>
-              <input style={{width:"100%", 
-                             height: "100%",
-                     }} 
-                     type="file" />
+
+              {directory ? 
+                <label htmlFor="input-area" className="custom-file-upload">
+                  {directoryName}
+                </label>
+                : 
+                <label htmlFor="input-area" className="custom-file-upload">
+                  Click or drag here to upload your directory!
+                </label>
+              }
+              <input type="file" ref={ref} id='input-area' onChange={(e)=>{handleFileInput(e)}} />
             </Box>
           : 
           <Box sx={{height: "100%",
