c40d8e8db901fcdc851ab1278052f0f6f1d697ca
Alex Monteverde <55471655+Amonteverde04@users.noreply.github.com>
2023-05-29 00:49:18 -0400

    Adding logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { ParticleBg } from './components/ParticleBg';
 import './App.css';
 import Menu from './components/Menu';
+import GhostLogo from './assets/GhostLogo.svg'
 
 function App() {
   return (
     <div>
       <ParticleBg />
+      <img src={GhostLogo} className='Logo' alt="Ghost Logo" />
       <Menu />
     </div>
   );
