410ff587fd7eb95628399a4d9710135cd58bb835
Alex Monteverde <alexmo@avi.com>
2023-05-29 22:05:40 -0400

    Updating styles and table

--- ui/src/components/AccordianTable.tsx
+++ ui/src/components/AccordianTable.tsx
@@ -13,91 +13,48 @@ import Paper from '@mui/material/Paper';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 
-function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number,
-  price: number,
-) {
-  return {
-    name,
-    calories,
-    fat,
-    carbs,
-    protein,
-    price,
-    history: [
-      {
-        date: '2020-01-05',
-        customerId: '11091700',
-        amount: 3,
-      },
-      {
-        date: '2020-01-02',
-        customerId: 'Anonymous',
-        amount: 1,
-      },
-    ],
-  };
+export interface rowData {
+  token: string;
+  createdDate: string;
 }
 
-function Row(props: { row: ReturnType<typeof createData> }) {
+const rows = [
+    {token: "123", createdDate: "12343"},
+    {token: "123", createdDate: "12343"},
+    {token: "123", createdDate: "12343"},
+    {token: "123", createdDate: "12343"},
+];
+
+function Row(props: { row: rowData }) {
     const { row } = props;
     const [open, setOpen] = React.useState(false);
   
     return (
       <React.Fragment>
         <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
-          <TableCell>
+          <TableCell component="th" scope="row">
             <IconButton
               aria-label="expand row"
               size="small"
               onClick={() => setOpen(!open)}
             >
-              {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+                {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
             </IconButton>
-          </TableCell>
-          <TableCell component="th" scope="row">
-            {row.name}
-          </TableCell>
-          <TableCell align="right">{row.calories}</TableCell>
-          <TableCell align="right">{row.fat}</TableCell>
-          <TableCell align="right">{row.carbs}</TableCell>
-          <TableCell align="right">{row.protein}</TableCell>
+            </TableCell>
+            <TableCell component="th" scope="row">
+                {row.token}
+            </TableCell>
+            <TableCell component="th" scope="row">
+                {row.createdDate}
+            </TableCell>
         </TableRow>
         <TableRow>
-          <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+          <TableCell style={{ paddingBottom: 0, paddingTop: 0}} colSpan={6} className={`${open ? "collapsable-cell--opened" : "collapsable-cell--collapsed"}`}>
             <Collapse in={open} timeout="auto" unmountOnExit>
               <Box sx={{ margin: 1 }}>
-                <Typography variant="h6" gutterBottom component="div">
+                <Typography variant="h6" gutterBottom component="div" color="red">
                   History
                 </Typography>
-                <Table size="small" aria-label="purchases">
-                  <TableHead>
-                    <TableRow>
-                      <TableCell>Date</TableCell>
-                      <TableCell>Customer</TableCell>
-                      <TableCell align="right">Amount</TableCell>
-                      <TableCell align="right">Total price ($)</TableCell>
-                    </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    {row.history.map((historyRow) => (
-                      <TableRow key={historyRow.date}>
-                        <TableCell component="th" scope="row">
-                          {historyRow.date}
-                        </TableCell>
-                        <TableCell>{historyRow.customerId}</TableCell>
-                        <TableCell align="right">{historyRow.amount}</TableCell>
-                        <TableCell align="right">
-                          {Math.round(historyRow.amount * row.price * 100) / 100}
-                        </TableCell>
-                      </TableRow>
-                    ))}
-                  </TableBody>
-                </Table>
               </Box>
             </Collapse>
           </TableCell>
@@ -105,14 +62,6 @@ function Row(props: { row: ReturnType<typeof createData> }) {
       </React.Fragment>
     );
 }
-
-const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0, 3.99),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3, 4.99),
-    createData('Eclair', 262, 16.0, 24, 6.0, 3.79),
-    createData('Cupcake', 305, 3.7, 67, 4.3, 2.5),
-    createData('Gingerbread', 356, 16.0, 49, 3.9, 1.5),
-];
 
 export default function AccordianTable() {
     return (
@@ -123,15 +72,12 @@ export default function AccordianTable() {
                   <TableRow>
                     <TableCell />
                     <TableCell>Token</TableCell>
-                    <TableCell align="right">Calories</TableCell>
-                    <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                    <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                    <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                    <TableCell>Date</TableCell>
                   </TableRow>
                 </TableHead>
                 <TableBody>
                   {rows.map((row) => (
-                    <Row key={row.name} row={row} />
+                    <Row key={row.token} row={row} />
                   ))}
                 </TableBody>
               </Table>
